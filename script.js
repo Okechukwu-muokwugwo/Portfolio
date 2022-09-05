97063e88e4109ff7f618a4897f36a8d6cbd8477e
Apt2tell <okei.muokwugwo@gmail.com>
2022-09-05 16:02:18 +0100

    Update the script to prevent the overlay to appear onpage load

--- script.js
+++ script.js
@@ -5,12 +5,10 @@ Toggle function for the Overlay Mobile menu
 */
 
 const overlay = document.getElementById('overlay1');
-const statusBar = document.getElementById('status');
 
 function toggleOverlay() {
-  overlay.style.display = 'block';
-  if (overlay.style.display === 'block') {
-    statusBar.style.display = 'none';
+  if (overlay.style.display === 'none') {
+    overlay.style.display = 'block';
   }
 }
 toggleOverlay();
