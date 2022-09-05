bfa95f752cb959c85bcffae16a8f97c39e8d40d9
Apt2tell <okei.muokwugwo@gmail.com>
2022-09-05 14:25:45 +0100

    Add script for the mobile menu

--- script.js
+++ script.js
@@ -8,12 +8,14 @@ const overlay = document.getElementById('overlay1');
 const statusBar = document.getElementById('status');
 
 function toggleOverlay() {
-    overlay.style.display = 'block';
-    if(overlay.style.display === 'block') {
-      statusBar.style.display = 'none';
-    } 
+  overlay.style.display = 'block';
+  if (overlay.style.display === 'block') {
+    statusBar.style.display = 'none';
+  }
 }
+toggleOverlay();
 
 function toggleOffOverlay() {
   overlay.style.display = 'none';
 }
+toggleOffOverlay();
