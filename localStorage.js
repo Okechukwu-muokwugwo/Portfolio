8eb1d05a57182a1e67621d5287e2fa948df02a03
Okechukwu <okei.muokwugwo@outlook.com>
2022-09-09 10:37:04 +0100

    Add localstorage javascript

---
+++ localStorage.js
@@ -0,0 +1,7 @@
+// const userName = document.getElementById('name');
+// const userEmail = document.getElementById('mail');
+// const userMsg = document.getElementById('message');
+
+// userName.addEventListener('change', populateLocalStorage);
+// userEmail.addEventListener('change', populateLocalStorage);
+// userMsg.addEventListener('change', populateLocalStorage);
