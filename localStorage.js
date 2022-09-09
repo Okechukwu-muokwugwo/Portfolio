8c311133ab99ddf39dd621172949cbcd84181e73
Tash Israel <47685927+tashisrael@users.noreply.github.com>
2022-09-09 12:45:03 +0300

    Add populateLocalStorage function

--- localStorage.js
+++ localStorage.js
@@ -1,7 +1,11 @@
 // const userName = document.getElementById('name');
 // const userEmail = document.getElementById('mail');
 // const userMsg = document.getElementById('message');
-
+// storing users'Data in the local storage
+const populateLocalStorage = () => {
+  const formData = { userName: userName.value, userEmail: userEmail.value, userMsg: userMsg.value };
+  window.localStorage.setItem('userData', JSON.stringify(formData));
+};
 // userName.addEventListener('change', populateLocalStorage);
 // userEmail.addEventListener('change', populateLocalStorage);
 // userMsg.addEventListener('change', populateLocalStorage);
