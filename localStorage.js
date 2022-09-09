b82d4a9e657b8d7f8b7d83c2f3f509c4ae769be1
Isiteketo Mutau <mutau85@gmail.com>
2022-09-09 12:10:08 +0200

    Add fetchData function

--- localStorage.js
+++ localStorage.js
@@ -6,6 +6,14 @@ const populateLocalStorage = () => {
   const formData = { userName: userName.value, userEmail: userEmail.value, userMsg: userMsg.value };
   window.localStorage.setItem('userData', JSON.stringify(formData));
 };
+// Retrieving the stored data in local storage
+const fetchData = () => {
+  const storageData = JSON.parse(window.localStorage.getItem('userData'));
+  userName.value = storageData.userName;
+  userEmail.value = storageData.userEmail;
+  userMsg.value = storageData.userMsg;
+};
+fetchData();
 // userName.addEventListener('change', populateLocalStorage);
 // userEmail.addEventListener('change', populateLocalStorage);
 // userMsg.addEventListener('change', populateLocalStorage);
